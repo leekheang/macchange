1fd9402bd1e6442b956da129cb41ec09196abd5e
leekheang <ly.kheang.jr@gmail.com>
2019-06-02 08:42:30 -0700

    2 mknewfunc

--- macchange.py
+++ macchange.py
@@ -23,16 +23,19 @@ def change_mac(interface, mac):
     subprocess.call(["sudo","ifconfig", interface ,"hw" ,"ether", mac ])
     subprocess.call(["sudo","ifconfig", interface ,"up"])
 
+def get_current_mac(interface):
+
+    ifconfig_result = str(subprocess.check_output(["ifconfig", interface]))
+
+    mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
+
+    if mac_address_search_result:
+        return mac_address_search_result.group(0)
+    else: 
+        print("[-] Could not read MAC address.")   
+
 options = get_arguments()
-change_mac(options.interface, options.mac)
-
-ifconfig_result = str(subprocess.check_output(["ifconfig", options.interface]))
-print(ifconfig_result) 
-
-
-mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
-
-if mac_address_search_result:
-    print("\n\n"+ mac_address_search_result.group(0))
-else: 
-    print("[-] Could not read MAC address.")    
+current_mac = get_current_mac(options.interface)
+print("Current MAC = " + str(current_mac))
+#change_mac(options.interface, options.mac)
+ 
